e792ccac5d8929e0fb54c2b96369d27e23f06b6e
seiya-Unity <pazu70@gmail.com>
2022-04-16 10:39:15 +0900

    0.01

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css?<?php echo date("Ymd:h-is"); ?>" rel="stylesheet" >
+    <title>総合</title>
+</head>
+<body>
+<header>
+    <div class="header-top">
+    <h1>総合SNS</h1>
+    </div>
+    <div class="header-sab">
+    <p>Twitterとインスタを同時に見ることや、投稿もできます。</p>
+        
+</header>
+<main>
+
+</body>
+</html>
