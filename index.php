c93a5c1b6d197fe8d272e60dbe166808547560d6
seiya-Unity <pazu70@gmail.com>
2022-04-24 20:41:49 +0900

    0.004

--- index.php
+++ index.php
@@ -8,27 +8,56 @@
     <title>GameSns</title>
 </head>
 <body>
+
 <header class="header-main">
+<h1>GameSns</h1>
     <div class="header-top">
-        <h1>GameSns</h1>
-    <from class="header-form" action="url" method="POST">
-        <label for="nickname">ゲーム名</label>
-        <input type="search" name="search">
-        <select name="format">
-            <option value="formatnone">形式未選択</option>
-            <option value="single">シングル</option>
-            <option value="duo">デュオ</option>
-            <option value="squat">スクワット</option>
-            <option value="quintet">クインテット</option>
-        </select>
-        <input type="datetime-local" name="time">
-        <button type="submit">🔎</button>
-    </from>
+        
+        <a href="login.php"><img class="icon" src="image/aicon.jpg" alt="アイコン"></a>
     </div>
     
     
 </header>
 <main>
+    <div class="side-left">
+        <p>home</p>
+        <p>profile</p>
+        <p>DM</p>
+    </div>
+    <div class="main-home">
+        <div class="main-post">
+            <form action="index.php" method="POST">
+                <input type="search" name="search">
+                <select name="format">
+                    <option value="none">形式未選択</option>
+                    <option value="single">シングル</option>
+                    <option value="duo">デュオ</option>
+                    <option value="squat">スクワット</option>
+                    <option value="quintet">クインテット</option>
+                </select>
+                <input type="datetime-local" name="time" value="time">
+                <br>
+                <textarea name="tweet"value="test"maxlength="150"></textarea>
+                <input type="submit" value="送信">
+            </form>
+        
+        </div>
+        <div class="timefrom">
+            <?php $search =$_POST['search'];?>
+            <p><?php echo $search; ?></p>
+
+            <?php $format=$_POST['format'];?>
+            <p><?php echo $format; ?></p>
+
+            <?php $time=$_POST['time'];?>
+            <p><?php echo $time; ?></p>
+
+            <?php $tweet=$_POST['tweet'];?>
+            <p><?php echo $tweet; ?></p>
+        </div>
+
+
+    </div>
     
 </main>
 </body>
