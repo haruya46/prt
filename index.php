90d7d7b7e96d002c0ae89bd9cf9a498bd1d13e00
seiya-Unity <pazu70@gmail.com>
2022-06-23 21:56:53 +0900

    新規作成

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
-<?php 
-require_once('dbc.php');
-?>
-
 <!DOCTYPE html>
-<html lang="jp">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,64 +8,6 @@ require_once('dbc.php');
     <title>GameSns</title>
 </head>
 <body>
-
-<header class="header-main">
-    <h1>GameSns</h1>
-    <div class="header-main-dm">
-        
-    </div>
-</header>
-<main>
-    <div class="side-left">
-        <p>home</p>
-        <a href="login.php"><img class="icon" src="image/aicon.jpg" alt="アイコン"></a>
-        <p>DM</p>
-        <p>大会を<br>探す</p>
-    </div>
-    <div class="main-home">
-        <div class="main-post">
-            <p>投稿</p>
-            <form action="index.php" method="POST">
-                <input type="text" name="gamename">
-                <select name="format">
-                    <option value="シングル">シングル</option>
-                    <option value="デュオ">デュオ</option>
-                    <option value="スクワット">スクワット</option>
-                    <option value="クインテット">クインテット</option>
-                </select>
-                <input type="datetime-local" name="time" value="time">
-                <br>
-                <textarea name="tweet"value="test"maxlength="150"></textarea>
-                <input type="submit" value="送信">
-
-            </form>
-            <?php require_once('formtest.php'); ?>
-        
-        </div>
-        <div class=timeline>
-            <p>タイムライン</p>
-            <?php require_once('tweetText.php') ?>
-            <div class= search-timeline>
-                <img class=icon src="image/aicon.jpg" alt="aicon">
-            
-                <p >ゲーム名<span>
-
-                </span></p>
-                <p>荒野行動</p>  
-            </div>
-            <div class=search-timeline>
-                <p>日付</p>
-                <p>xxxx年xx月xx日</p>   
-            </div>
-            <?php 
-                require_once('tweetText.php');
-                echo $tweet;
-                ?>
-            <p maxlength="150">ここにテキストここにテキストここにテキストここにテキスト</p>
-        </div>
-        
-    </div>
     
-</main>
 </body>
 </html>
